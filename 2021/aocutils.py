0248b8db185220cdf9e2ee094c2b3334120fe1ac
Ole Petter <ole.orhagen@northern.tech>
2021-12-14 20:16:09 +0100

    Clean up the utils
    Changelog: None
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- 2021/aocutils.py
+++ 2021/aocutils.py
@@ -2,15 +2,17 @@
 #
 # Thank you, Peter Norvig!
 #
+# TODO - add the foldl and foldr functions here!
 import re
 import numpy as np
 import math
 import urllib.request
 
 from collections import Counter, defaultdict, namedtuple, deque
-from functools   import lru_cache
-from itertools   import permutations, combinations, chain, cycle, product, islice
-from heapq       import heappop, heappush
+from functools import lru_cache
+from itertools import permutations, combinations, chain, cycle, product, islice
+from heapq import heappop, heappush
+
 
 def Input(day):
     "Open this day's input file."
@@ -21,30 +23,38 @@ def Input(day):
         return urllib.request.urlopen("http://norvig.com/ipython/" + filename)
 
 
-def data(fname, parser=str, sep='\n') -> list:
+def data(fname, parser=str, sep="\n") -> list:
     "Split the day's input file into sections separated by `sep`, and apply `parser` to each."
     sections = open(fname).read().rstrip().split(sep)
     return [parser(section) for section in sections]
 
-def transpose(matrix): return zip(*matrix)
 
-def first(iterable): return next(iter(iterable))
+def transpose(matrix):
+    return zip(*matrix)
+
+
+def first(iterable):
+    return next(iter(iterable))
+
 
 def nth(iterable, n, default=None):
     "Returns the nth item of iterable, or a default value"
     return next(islice(iterable, n, None), default)
 
-cat = ''.join
 
-Ø   = frozenset() # Empty set
-inf = float('inf')
+cat = "".join
+
+Ø = frozenset()  # Empty set
+inf = float("inf")
 BIG = 10 ** 999
+
 
 def grep(pattern, lines):
     "Print lines that match pattern."
     for line in lines:
         if re.search(pattern, line):
             print(line)
+
 
 def groupby(iterable, key=lambda it: it):
     "Return a dic whose keys are key(it) and whose values are all the elements of iterable with that key."
@@ -53,49 +63,73 @@ def groupby(iterable, key=lambda it: it):
         dic[key(it)].append(it)
     return dic
 
+
 def powerset(iterable):
     "Yield all subsets of items."
     items = list(iterable)
-    for r in range(len(items)+1):
+    for r in range(len(items) + 1):
         for c in combinations(items, r):
             yield c
 
-# 2-D points implemented using (x, y) tuples
-def X(point): return point[0]
-def Y(point): return point[1]
 
-def neighbors4(point):
+# 2-D points implemented using (x, y) tuples
+def X(point):
+    return point[0]
+
+
+def Y(point):
+    return point[1]
+
+
+def neighbours4(point):
     "The four neighbors (without diagonals)."
     x, y = point
-    return ((x+1, y), (x-1, y), (x, y+1), (x, y-1))
+    return ((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1))
 
-def neighbors8(point):
+
+def neighbours8(point):
     "The eight neighbors (with diagonals)."
     x, y = point
-    return ((x+1, y), (x-1, y), (x, y+1), (x, y-1),
-            (x+1, y+1), (x-1, y-1), (x+1, y-1), (x-1, y+1))
+    return (
+        (x + 1, y),
+        (x - 1, y),
+        (x, y + 1),
+        (x, y - 1),
+        (x + 1, y + 1),
+        (x - 1, y - 1),
+        (x + 1, y - 1),
+        (x - 1, y + 1),
+    )
+
 
 def cityblock_distance(p, q=(0, 0)):
     "City block distance between two points."
     return abs(X(p) - X(q)) + abs(Y(p) - Y(q))
 
+
 def euclidean_distance(p, q=(0, 0)):
     "Euclidean (hypotenuse) distance between two points."
     return math.hypot(X(p) - X(q), Y(p) - Y(q))
 
+
 def trace1(f):
     "Print a trace of the input and output of a function on one line."
+
     def traced_f(*args):
         result = f(*args)
-        print('{}({}) = {}'.format(f.__name__, ', '.join(map(str, args)), result))
+        print("{}({}) = {}".format(f.__name__, ", ".join(map(str, args)), result))
         return result
+
     return traced_f
+
 
 def astar_search(start, h_func, moves_func):
     "Find a shortest sequence of states from start to a goal state (a state s with h_func(s) == 0)."
-    frontier  = [(h_func(start), start)] # A priority queue, ordered by path length, f = g + h
-    previous  = {start: None}  # start state has no previous state; other states will
-    path_cost = {start: 0}     # The cost of the best path to a state.
+    frontier = [
+        (h_func(start), start)
+    ]  # A priority queue, ordered by path length, f = g + h
+    previous = {start: None}  # start state has no previous state; other states will
+    path_cost = {start: 0}  # The cost of the best path to a state.
     while frontier:
         (f, s) = heappop(frontier)
         if h_func(s) == 0:
@@ -108,6 +142,19 @@ def astar_search(start, h_func, moves_func):
                 previous[s2] = s
     return dict(fail=True, front=len(frontier), prev=len(previous))
 
+
 def Path(previous, s):
     "Return a list of states that lead to state s, according to the previous dict."
-    return ([] if (s is None) else Path(previous, previous[s]) + [s])
+    return [] if (s is None) else Path(previous, previous[s]) + [s]
+
+
+###########################
+# Matrix initialization #
+###########################
+
+
+def zeromat(size):
+    return np.zeros(size, dtype=int)
+
+def idmat(size):
+    return np.ones(size, dtype=int)
