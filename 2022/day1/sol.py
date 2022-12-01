2a2ac880d11f93e35d3aaaa9872ecabbc9aac879
Ole Petter <ole.orhagen@northern.tech>
2022-12-01 13:39:47 +0100

    Clean up day 1
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- 2022/day1/sol.py
+++ 2022/day1/sol.py
@@ -8,16 +8,7 @@ puzzle = Puzzle(year=2022, day=1)
 
 # Handy utility parser
 import parse as p
-
 from aocutils import *
-
-def solve_p1(d):
-    puzzle.answer_a =  max(map(sum, d))
-
-def solve_p2(d):
-    answer = sum(sorted(map(sum,d))[-3:])
-    puzzle.answer_b = answer
-
 
 # Test case
 d = data(Input('part1.test'), parser=ints, sep="\n\n")
@@ -26,11 +17,5 @@ assert max(map(sum, d)) == 24000
 
 # Parse the actual problem data
 d = data(puzzle.input_data, parser=ints, sep="\n\n")
-
-solve_p1(d)
-
-solve_p2(d)
-
-
-
-# puzzle.answer_a = 0
+puzzle.answer_a =  max(map(sum, d))
+puzzle.answer_b = sum(sorted(map(sum,d))[-3:])
