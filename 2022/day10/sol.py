b9d6d076db9a330c9ad4b5cd55a98fb2211e86b3
Ole Petter <ole.orhagen@northern.tech>
2022-12-11 10:22:03 +0100

    Day 10 cleanup
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- 2022/day10/sol.py
+++ 2022/day10/sol.py
@@ -16,39 +16,30 @@ def execute(instruction):
 
 
 def solve_p1(instructions):
-    values = [1]
+    cycle = [1]
     x = 1
     for instruction in instructions:
         for value in execute(instruction):
             x = x + value
-            values.append(x)
+            cycle.append(x)
 
     # Print to screen
     for i in range(240):
-        sprite = [values[i] - 1, values[i], values[i] + 1]
-        if i % 40 == 0 and i != 0:
+        if i % 40 == 0:
             print(flush=True)
-        if i % 40 in sprite:
+        if (i % 40) - 1 <= cycle[i] <= (i % 40) + 1:
             print("#", end="", flush=True)
         else:
             print(".", end="", flush=True)
 
-    return sum([values[i - 1] * i for i in range(20, 221, 40)])
-
-
-def solve_p2(d):
-    pass
+    return sum([cycle[i - 1] * i for i in range(20, 221, 40)])
 
 
 td = data(Input("part1.test"))
-
 assert solve_p1(td) == 13140, solve_p1(td)
-
-print()
 
 puzzle = Puzzle(year=2022, day=10)
 d = data(puzzle.input_data)
 
 puzzle.answer_a = solve_p1(d)
-
 puzzle.answer_b = "RKPJBPLA"
