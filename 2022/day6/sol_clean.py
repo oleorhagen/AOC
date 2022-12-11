43305b1c2cdcf5ac0b7174bec49dbee013cacae5
Ole Petter <ole.orhagen@northern.tech>
2022-12-11 10:22:19 +0100

    format day 6
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- 2022/day6/sol_clean.py
+++ 2022/day6/sol_clean.py
@@ -1,11 +1,12 @@
 import sys
+
 sys.path.append("../../aocutils")
 
 from aocd.models import Puzzle
 
 from aocutils import *
 
-f = lambda stream, window: first([i + window for i in range(len(stream)-window) if len(set(stream[i:i+window])) == window])
+f = lambda stream, window: first([i + window for i in range(len(stream) - window) if len(set(stream[i : i + window])) == window])
 
 test_letters = tokens("[a-zA-Z]")(open("part1.test").read())
 assert f(test_letters, 4) == 7
@@ -15,3 +16,4 @@ puzzle = Puzzle(year=2022, day=6)
 puzzle.answer_a = f(tokens("[a-z]")(puzzle.input_data), window=4)
 
 puzzle.answer_b = f(tokens("[a-z]")(puzzle.input_data), window=14)
+
