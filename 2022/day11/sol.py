cc05c4ac3a7e5cf77a8aae4fea3f57629ee6c44c
Ole Petter <ole.orhagen@northern.tech>
2022-12-11 14:23:11 +0100

    Clean up day 11 and part B added
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- 2022/day11/sol.py
+++ 2022/day11/sol.py
@@ -19,62 +19,32 @@ def parser(d):
     return monkey
 
 
-def solve_p1(monkeys):
-    # Do the rounds
-    for round in range(20):
-        for monkey_index in range(len(monkeys)):
+def solve(monkeys, rounds=20, level_normalizer=lambda x: x // 3):
+    for round in range(rounds):
+        for monkey in monkeys:
             # Inspect the item
-            new_items = []
-            for item in monkeys[monkey_index]["items"]:
-                monkeys[monkey_index]["inspection_count"] += 1
-                new_worry_level = monkeys[monkey_index]["operation"](item) // 3
-                if new_worry_level % monkeys[monkey_index]["test"] == 0:
-                    monkeys[monkeys[monkey_index]["to_monkey_true"]]["items"].append(
-                        new_worry_level
-                    )
+            for item in monkey["items"]:
+                monkey["inspection_count"] += 1
+                new_worry_level = level_normalizer(monkey["operation"](item))
+                if new_worry_level % monkey["test"] == 0:
+                    monkeys[monkey["to_monkey_true"]]["items"].append(new_worry_level)
                 else:
-                    monkeys[monkeys[monkey_index]["to_monkey_false"]]["items"].append(
-                        new_worry_level
-                    )
-            monkeys[monkey_index]["items"] = []
+                    monkeys[monkey["to_monkey_false"]]["items"].append(new_worry_level)
+            monkey["items"] = []
 
     inspection_counts = sorted([m["inspection_count"] for m in monkeys], reverse=True)
     return inspection_counts[0] * inspection_counts[1]
 
 
-def print_monkeys(monkeys):
-    # for i, m in enumerate(monkeys):
-    #     print("Monkey: ", i)
-    #     print(m)
-    for i, m in enumerate(monkeys):
-        print(f"Monkey: {i} inspected items: {m['inspection_count']}")
-    print()
+def solve_p1(monkeys):
+    return solve(monkeys)
 
 
 def solve_p2(monkeys):
     # Do the rounds
-    for round in range(5):
-        print_monkeys(monkeys)
-        for monkey_index in range(len(monkeys)):
-            # Inspect the item
-            new_items = []
-            for item in monkeys[monkey_index]["items"]:
-                monkeys[monkey_index]["inspection_count"] += 1
-                new_worry_level = monkeys[monkey_index]["operation"](item)
-                if new_worry_level % monkeys[monkey_index]["test"] == 0:
-                    monkeys[monkeys[monkey_index]["to_monkey_true"]]["items"].append(
-                        new_worry_level
-                        % monkeys[monkeys[monkey_index]["to_monkey_true"]]["test"]
-                    )
-                else:
-                    monkeys[monkeys[monkey_index]["to_monkey_false"]]["items"].append(
-                        new_worry_level
-                        % monkeys[monkeys[monkey_index]["to_monkey_false"]]["test"]
-                    )
-            monkeys[monkey_index]["items"] = []
-
-    # inspection_counts = sorted([m["inspection_count"] for m in monkeys], reverse=True)
-    # return inspection_counts[0] * inspection_counts[1]
+    cf = math.prod(m["test"] for m in monkeys)
+    print("CF: ", cf)
+    return solve(monkeys, rounds=10 * 1000, level_normalizer=lambda x: x % cf)
 
 
 td = data(Input("part1.test"), parser=parser, sep="\n\n")
@@ -88,9 +58,8 @@ puzzle.answer_a = solve_p1(d)
 
 td = data(Input("part1.test"), parser=parser, sep="\n\n")
 
-solve_p2(td)
+assert solve_p2(td) == 2713310158
 
+d = data(puzzle.input_data, parser=parser, sep="\n\n")
 
-# assert solve_p2(td) == 0
-
-# puzzle.answer_b = solve_p2(d)
+puzzle.answer_b = solve_p2(d)
